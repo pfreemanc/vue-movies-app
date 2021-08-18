949a8bc19efa90f71d229b0afa246a5bbe77818f
pfreemanc <pfreemanc@gmail.com>
2021-08-18 18:16:15 -0400

    Added router functionality

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Welcome to this movie thing. WIP. Obviously</h1>
+    <router-link to="/search" class="button">Begin Whatever This Is</router-link>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  },
+};
+</script>
